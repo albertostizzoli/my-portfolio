b3e4a7b04cfe4e3b5393e05950e8cee6e5511cb9
albertostizzoli <albertostizzoli@yahoo.it>
2024-05-16 14:39:00 +0200

    scaffolding base and test

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-center text-bg-danger ">Hello World</h1>
+    <h2>Ciao mi chiamo Alberto</h2>
+    <i class="fa-solid fa-user"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
