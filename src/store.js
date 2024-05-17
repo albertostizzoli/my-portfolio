bdb6412d00375520e739da481a33a25586a65212
albertostizzoli <albertostizzoli@yahoo.it>
2024-05-17 09:46:16 +0200

    added languages in store

--- src/store.js
+++ src/store.js
@@ -2,4 +2,47 @@ import { reactive } from "vue";
 
 export const store = reactive({
 
+    languages: [
+        {
+            name: "HTML5",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
+        },
+        {
+            name: "CSS3",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
+        },
+        {
+            name: "Bootstrap",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg"
+        },
+        {
+            name: "JavaScript",
+            icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+        },
+        {
+            name: "VueJS",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vuejs-colored.svg"
+        },
+        {
+            name: "SASS",
+            icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
+        },
+        {
+            name: "PHP",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/php-colored.svg"
+        },
+        {
+            name: "MySQL",
+            icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+        },
+        {
+            name: "Laravel",
+            icon: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/laravel-colored.svg"
+        },
+        {
+            name: "JAVA",
+            icon: "https://www.vectorlogo.zone/logos/java/java-icon.svg"
+        },
+    ],
+
 });
