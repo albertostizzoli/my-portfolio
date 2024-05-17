120ceb3c01610fc7a892e52456aec85440dc77cb
albertostizzoli <albertostizzoli@yahoo.it>
2024-05-17 10:32:52 +0200

    added experiences in store

--- src/store.js
+++ src/store.js
@@ -45,4 +45,31 @@ export const store = reactive({
         },
     ],
 
+    experiences: [
+        {
+            title: "Experis Academy",
+            name: "Java Developer",
+            period: "Marzo 2024 - Maggio 2024",
+            descritpion: "Percorso formativo di 6 settimane in cui ho avuto modo di approfondire i fondamentali dell'OOP tramite il linguaggio JAVA, il tutto seguendo lezioni di teoria ed esercitazioni pomeridiane svolte in autonomia",
+        },
+        {
+            title: "Boolean",
+            name: "Jr Full Stack Web Developer Trainee",
+            period: "Agosto 2023 - Febbraio 2024",
+            descritpion: "Esperienza di 700 ore dal forte approccio pratico studiando tecnologie sia dal lato Front-End che dal lato Back-End. Questo mi ha permesso di sviluppare il mindset di programmatore, affinità al problem-solving, ma anche di collaborare in team per lo sviluppo di un sito web completo, acquisendo competenze di collaborazione e gestione delle scadenze."
+        },
+        {
+            title: "Stizzoli S.P.A",
+            name: "Addetto al magazzino filatura / Smacchinatore di macchine da maglieria (Shima Seiki)",
+            period: "Marzo 2014 - Setembre 2019",
+            descritpion:
+            "<ul class=\"ps-4\">" +
+            "<li>Responsabile della preparazione delle serie di capi da mandare in produzione e controllo dell’entrata e uscita delle materie prime attraverso l’azienda;</li>" +
+            "<li>Risolvere problemi legati alla produzione</li>" +
+            "<li>Responsabile della produzione di serie di capi con controllo qualità e quantità del prodotto in modo da poter rispettare le esigenze del cliente</li>" +
+            "<li>Manutenzione e pulizia durante la stagione estiva delle macchine da maglieria;</li>" +
+            "</ul>"
+        },
+    ],
+
 });
