02e11f530606d893d99fcd06fa4b362a10725831
albertostizzoli <albertostizzoli@yahoo.it>
2024-05-18 14:31:31 +0200

    create Header

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-
-    </div>
+    <header>
+        <nav class="navbar">
+            <div id="navbar-list">
+                <ul>
+                    <li><span class="index">.01</span>Chi sono</li>
+                    <li><span class="index">.02</span>Esperienze</li>
+                    <li><span class="index">.03</span>I miei Progetti</li>
+                </ul>
+            </div>
+            <div id="side-bar">
+                <button class="sidebar-btn" :class="is_active_menu ? 'active' : ''" @click="isActive()"
+                data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                <span class="top"></span>
+                <span class="mid"></span>
+                <span class="bottom"></span>
+            </button>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script>
