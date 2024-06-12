c1d0fae06e1ee3a01567424f7b593c54eb945b65
albertostizzoli <albertostizzoli@yahoo.it>
2024-06-12 10:55:07 +0200

    create file LoaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
+  <div v-if="loading">
+    <LoaderComponent></LoaderComponent>
+  </div>
   <HeaderSection></HeaderSection>
   <main>
     <AppWrapper></AppWrapper>
     <router-view></router-view>
   </main>
-  
+
   <FooterSection></FooterSection>
 </template>
 
 <script>
+import LoaderComponent from './components/LoaderComponent.vue';
 import HeaderSection from './components/HeaderSection.vue';
 import AppWrapper from './components/AppWrapper.vue';
 import FooterSection from './components/FooterSection.vue';
-  export default {
-    components: {
-      HeaderSection,
-      AppWrapper,
-      FooterSection,
+export default {
+  components: {
+    LoaderComponent,
+    HeaderSection,
+    AppWrapper,
+    FooterSection,
+  },
+  data() {
+    return {
+      loading: true,
     }
-    
+  },
+  methods: {
+    loader() {
+      setTimeout(() => {
+        this.loading = false
+      }, 5500)
+    }
+  },
+  mounted(){
+    this.loader();
   }
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
