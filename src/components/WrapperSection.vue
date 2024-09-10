9be362c67159c266550b03d5de625f1d1799cd40
albertostizzoli <albertostizzoli@yahoo.it>
2024-09-10 11:37:23 +0200

    change CV #6

--- src/components/WrapperSection.vue
+++ src/components/WrapperSection.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
         <ul>
-            <li><a href="https://drive.google.com/file/d/1eHZbFA1OrMe9dRk4U4jnXgXwHnnunlyQ/view?usp=drive_link"><i class="fa-solid fa-file fa-3x"></i></a></li>
+            <li><a href="https://drive.google.com/file/d/1ykPkZXlDm1glXKqZ0Av7c5ajRM5rpBE-/view?usp=drive_link"><i class="fa-solid fa-file fa-3x"></i></a></li>
             <li><a href="https://github.com/albertostizzoli"><i class="fa-brands fa-github fa-3x"></i></a></li>
             <li><a href="https://www.linkedin.com/in/alberto-stizzoli-82a676260/"><i class="fa-brands fa-linkedin-in fa-3x"></i></a></li>
         </ul>
